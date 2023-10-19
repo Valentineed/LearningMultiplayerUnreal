538999a8882e81344138624708a383f93b46afc5
Valentin Edouard Emile Dol <49204480+Valentineed@users.noreply.github.com>
2023-10-18 20:32:43 -0400

    first commit

---
+++ Source/LearningMultiplayer/LearningMultiplayerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LearningMultiplayerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALearningMultiplayerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALearningMultiplayerGameMode();
+};
+
+
+
