1d30a749e00b3af3f80a45d15ab15a1992b66e4b
Valentin Dol <dol.valentin@hotmail.com>
2023-11-02 14:10:05 -0400

    add rocket

--- Tiros/Source/Tiros/Weapon/ProjectileRocket.cpp
+++ Tiros/Source/Tiros/Weapon/ProjectileRocket.cpp
@@ -3,3 +3,28 @@
 
 #include "ProjectileRocket.h"
 
+#include "Kismet/GameplayStatics.h"
+
+
+AProjectileRocket::AProjectileRocket()
+{
+	RocketMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RocketMesh"));
+	RocketMesh->SetupAttachment(RootComponent);
+	RocketMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AProjectileRocket::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                              FVector NormalImpulse, const FHitResult& Hit)
+{
+	if(const APawn* FiringPawn = GetInstigator())
+	{
+		if(AController* FiringController = FiringPawn->GetController())
+		{
+			UGameplayStatics::ApplyRadialDamageWithFalloff(this, Damage,MinimumDamage, GetActorLocation(), DamageInnerRadius,
+				DamageOuterRadius, 1.f,UDamageType::StaticClass(),TArray<AActor*>(), this, FiringController);
+		}
+		
+	}
+	
+	Super::OnHit(HitComp, OtherActor, OtherComp, NormalImpulse, Hit);
+}
--- Tiros/Source/Tiros/Weapon/ProjectileRocket.h
+++ Tiros/Source/Tiros/Weapon/ProjectileRocket.h
@@ -13,5 +13,20 @@ UCLASS()
 class TIROS_API AProjectileRocket : public AProjectile
 {
 	GENERATED_BODY()
+public:
+	AProjectileRocket();
+protected:
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	float MinimumDamage = 10.f;
 	
+	UPROPERTY(EditDefaultsOnly)
+	float DamageInnerRadius = 200.f;
+	
+	UPROPERTY(EditDefaultsOnly)
+	float DamageOuterRadius = 500.f;
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent*  RocketMesh;
 };
